22f4a9514faf4e92ecf52299167f73cde46873a0
Sonui <me@sonui.cn>
2022-05-07 19:59:29 +0800

    fear: 初始化雪花ID生成器

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 	"github.com/gin-gonic/gin"
 	"sonui.cn/cloudprint/models"
 	"sonui.cn/cloudprint/pkg/conf"
+	"sonui.cn/cloudprint/pkg/utils"
 	"sonui.cn/cloudprint/routers"
 )
 
 func main() {
+	// TODO: 对配置类进行重构，使用VIPER库
 	// 初始化配置
-	conf.Type = "ENV"
-	err := conf.InitConfig("config.yaml")
-	if err != nil {
-		fmt.Println("config init error:", err)
-		return
+	conf.Type = "YAML"
+	if err := conf.InitConfig("config.yaml"); err != nil {
+		panic(fmt.Sprintf("init config failed, err: %s", err.Error()))
+	}
+
+	// 初始化雪花ID生成器
+	if err := utils.NewWorker(conf.Conf.Config.Node); err != nil {
+		panic(fmt.Sprintf("init snowflake failed, err: %s", err.Error()))
 	}
 
 	//初始化数据库连接
