3aabc20da25fe9ccaaabcca651d74b784ba5d019
Sonui <me@sonui.cn>
2022-06-16 20:00:42 +0800

    del: 去除无用代码

--- main.go
+++ main.go
@@ -1,36 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"sonui.cn/cloudprint/models"
-	"sonui.cn/cloudprint/pkg/conf"
-	"sonui.cn/cloudprint/pkg/utils"
 	"sonui.cn/cloudprint/routers"
+	"sonui.cn/cloudprint/utils"
 )
 
 func main() {
-	// TODO: 对配置类进行重构，使用VIPER库
-	// 初始化配置
-	conf.Type = "YAML"
-	if err := conf.InitConfig("config.yaml"); err != nil {
-		panic(fmt.Sprintf("init config failed, err: %s", err.Error()))
-	}
-
-	// 初始化雪花ID生成器
-	if err := utils.NewWorker(conf.Conf.Config.Node); err != nil {
-		panic(fmt.Sprintf("init snowflake failed, err: %s", err.Error()))
-	}
-
-	//初始化数据库连接
-	models.Init()
-
 	// 初始化gin
 	gin.SetMode(gin.ReleaseMode)
 	api := routers.InitRouter()
-
 	// 启动服务
-	if err := api.Run(conf.Conf.Config.Listen); err != nil {
+	if err := api.Run(utils.Config.Run.Listen); err != nil {
 		panic(err)
 	}
 }
