8fa380bc96a205caa99db6f79bad630c4751e2c1
Sonui <me@sonui.cn>
2022-07-05 23:32:31 +0800

    perf: docx文件本地解析

--- services/file.go
+++ services/file.go
@@ -7,18 +7,19 @@ import (
 	"sonui.cn/cloudprint/models"
 	"sonui.cn/cloudprint/utils"
 	"sonui.cn/cloudprint/utils/log"
+	"sonui.cn/cloudprint/utils/openxml"
 	"time"
 )
 
 // SaveFile 保存文件至COS
 // name 文件名
-// path 本地文件路径
+// localPath 本地文件路径
 // info 文件信息
 // size 文件大小
 // body 文件内容
-func SaveFile(user, name, path, info string, size uint64) (*models.File, *log.ErrorInfo) {
+func SaveFile(user, name, localPath, info string, size uint64) (*models.File, *log.ErrorInfo) {
 	service := s3.New(utils.S3)
-	fp, _ := os.Open(path)
+	fp, _ := os.Open(localPath)
 	defer utils.FileClose(fp)
 
 	withContext, err := service.PutObject(&s3.PutObjectInput{
@@ -42,7 +43,13 @@ func SaveFile(user, name, path, info string, size uint64) (*models.File, *log.Er
 			UpdateTime: time.Time{},
 		}
 		var fInfo = models.FileInfo{}
-		fInfo.PageNum, err = utils.GetFilePagesNum(path)
+		// 判断是否为docx文件
+		if utils.CheckFileTypeBySuffix(name, "docx") && utils.CheckFileTypeByHeader(localPath, []byte("PK")) {
+			// 为docx文件，本地解析
+			fInfo.PageNum, err = openxml.GetDocxPages(localPath)
+		} else {
+			fInfo.PageNum, err = utils.GetFilePagesNum(localPath)
+		}
 
 		if err != nil {
 			return nil, log.NewError(101, err.Error())
