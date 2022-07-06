e38023a602163ad64c8226355df5ebd7351ffd8b
Sonui <me@sonui.cn>
2022-07-06 22:56:21 +0800

    feat: 获取请求发起用户

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"sonui.cn/cloudprint/utils/log"
 	"time"
 )
 
@@ -32,4 +35,42 @@ func (u *User) Create() error {
 func (u *User) Save() error {
 	u.UpdateTime = time.Now()
 	return DB.Model(u).Updates(u).Error
+}
+
+func GetUserFromRequest(c *gin.Context) (*User, *log.ErrorInfo) {
+	//获取cookie
+	var openid, session *http.Cookie
+	var err error
+	openid, err = c.Request.Cookie("openid")
+	if err == nil {
+		session, err = c.Request.Cookie("session")
+	}
+	if err != nil || openid.Value == "" || session.Value == "" {
+		return nil, log.NewError(301, "no login")
+	}
+
+	// 查询登录信息
+	var value struct {
+		Uid int64 `json:"uid"`
+	}
+
+	cache := Cache{
+		Key:   session.Value,
+		Value: value,
+	}
+
+	// 空记录
+	if err = cache.Find(); err != nil {
+		return nil, log.NewError(301, "no login")
+	}
+
+	// 查询用户信息
+	user := User{
+		Id: value.Uid,
+	}
+	if err = user.Find(); err != nil {
+		return nil, log.NewError(301, "no login")
+	}
+
+	return &user, nil
 }
--- routers/controllers/filter.go
+++ routers/controllers/filter.go
@@ -4,72 +4,32 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"sonui.cn/cloudprint/models"
-	"sonui.cn/cloudprint/vo"
 )
+
+var whiteList = []string{
+	"/api/v1/user/login",
+	"/api/v1/user/register",
+	"/api/v1/order/pay/notify",
+}
 
 func TokenHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// 特判路径是否为Login接口 是就放过
+		// 特判路径是否为白名单接口 是就放过
 		// 不可以模糊匹配
-		if c.Request.URL.Path == "/api/v1/user/login" {
-			c.Next()
-			return
+		for _, path := range whiteList {
+			if c.Request.URL.Path == path {
+				c.Next()
+				return
+			}
 		}
+		_, err := models.GetUserFromRequest(c)
 
-		var token vo.TokenVo
-		//获取cookie
-		ck, err := c.Request.Cookie("openid")
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"code": 301,
-				"msg":  "no login",
+			c.JSON(http.StatusOK, gin.H{
+				"code":    err.GetErrorCode(),
+				"message": err.GetErrorMsg(),
 			})
-			return
-		}
-		token.Openid = ck.Value
-
-		ck, err = c.Request.Cookie("session")
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"code": 301,
-				"msg":  "no login",
-			})
-			return
-		}
-		token.Session = ck.Value
-
-		if token.Openid == "" || token.Session == "" {
-			c.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"code": 301,
-				"msg":  "no login",
-			})
-			return
-		}
-
-		// 判断token是否过期
-		var value = &vo.TokenVo{}
-		cache := models.Cache{
-			Key:   token.Session,
-			Value: value,
-		}
-
-		err = cache.Find()
-		// 空记录
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"code": 301,
-				"msg":  "no login",
-			})
-			return
-		}
-
-		// 记录不一致
-		if !(value.Openid == token.Openid) {
-			// 无法验证token
-			c.AbortWithStatusJSON(http.StatusOK, gin.H{
-				"code": 301,
-				"msg":  "no login",
-			})
+			c.Abort()
 			return
 		}
 
